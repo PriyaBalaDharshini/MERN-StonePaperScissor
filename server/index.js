d5f4e13ef3d5fe28d60e11aefccd1575ef129571
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-09-25 13:53:30 +0530

    Backend setup done

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8001;
+const DB = process.env.DB_URL;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Welcome to Backend"));
+
+mongoose
+    .connect(DB)
+    .then(() => console.log("Database Connected Successfully"))
+    .catch((error) => console.log(error.message));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
