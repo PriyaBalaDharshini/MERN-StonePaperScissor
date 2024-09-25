034043e72a218497e25b718c68c6a9fa028ef284
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-09-25 14:02:57 +0530

    Controllers created

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const DB = process.env.DB_URL;
 app.use(express.json());
 app.use(cors());
 
+//Model for game
 const gameSchema = new mongoose.Schema({
     player1Name: String,
     player2Name: String,
@@ -20,6 +21,40 @@ const gameSchema = new mongoose.Schema({
     winner: String,
 });
 const Game = mongoose.model('Game', gameSchema);
+
+
+//Routes
+
+//to save-the-game
+app.post("/save-game", async (req, res) => {
+    try {
+
+        const { player1Name, player2Name, player1Score, player2Score, winner } = req.body;
+        const newGame = new Game({
+            player1Name,
+            player2Name,
+            player1Score,
+            player2Score,
+            winner,
+        });
+        await newGame.save();
+        res.status(200).json({ message: "Game data saved successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" }, error.message)
+    }
+
+})
+
+// Get all game data
+app.get('/all-games', async (req, res) => {
+    try {
+        const games = await Game.find();
+        res.status(200).send(games);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" }, error.message)
+    }
+
+});
 
 app.get("/", (req, res) => res.send("Welcome to Backend"));
 
